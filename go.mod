a7120f8d181e95cf7d3750b53f15f0f21e2e6d59
Yvan FEREZ <yvan.ferez@shadline.com>
2020-12-14 12:32:14 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module asset-transfer-basic
+
+go 1.14
+
+require (
+	github.com/google/uuid v1.1.2
+	github.com/hyperledger/fabric-sdk-go v1.0.0-rc1
+)
---
+++ main.go
@@ -0,0 +1,163 @@
+/*
+Copyright 2020 IBM All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/google/uuid"
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+)
+
+func main() {
+	log.Println("============ application-golang starts ============")
+
+	err := os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
+	if err != nil {
+		log.Fatalf("Error setting DISCOVERY_AS_LOCALHOST environemnt variable: %v", err)
+	}
+
+	wallet, err := gateway.NewFileSystemWallet("wallet")
+	if err != nil {
+		log.Fatalf("Failed to create wallet: %v", err)
+	}
+
+	if !wallet.Exists("appUser") {
+		err = populateWallet(wallet)
+		if err != nil {
+			log.Fatalf("Failed to populate wallet contents: %v", err)
+		}
+	}
+
+	ccpPath := filepath.Join(
+		"..",
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"connection-org1.yaml",
+	)
+
+	gw, err := gateway.Connect(
+		gateway.WithConfig(config.FromFile(filepath.Clean(ccpPath))),
+		gateway.WithIdentity(wallet, "appUser"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to connect to gateway: %v", err)
+	}
+	defer gw.Close()
+
+	network, err := gw.GetNetwork("chimitrace-channel")
+	if err != nil {
+		log.Fatalf("Failed to get network: %v", err)
+	}
+
+	contract := network.GetContract("chimitrace")
+
+	log.Println("--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger")
+	result, err := contract.SubmitTransaction("InitLedger")
+	if err != nil {
+		log.Fatalf("Failed to Submit transaction: %v", err)
+	}
+	log.Println(string(result))
+
+	transacUUID := uuid.New().String()
+
+	log.Printf("--> Submit Transaction: CreateAsset, creates new asset with ID (%s), color, owner, size, and appraisedValue arguments\n", transacUUID)
+	result, err = contract.SubmitTransaction("CreateAsset", transacUUID, "T", "5", "15", "300", "true", "3")
+	if err != nil {
+		log.Fatalf("Failed to Submit transaction: %v", err)
+	}
+	log.Println(string(result))
+
+	log.Println("--> Evaluate Transaction: ReadAsset, function returns an asset with a given assetID")
+	result, err = contract.EvaluateTransaction("ReadAsset", transacUUID)
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v\n", err)
+	}
+	log.Println(string(result))
+
+	log.Println("--> Submit Transaction: WithDrawAsset, updates 'a1da243e-42c6-4c2d-86fe-5c44d79ea20d' attributes")
+	result, err = contract.SubmitTransaction("WithDrawAsset", "a1da243e-42c6-4c2d-86fe-5c44d79ea20d", "3")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v", err)
+	}
+	log.Println(string(result))
+
+	log.Println("--> Evaluate Transaction: ReadAsset, function returns an asset with a given assetID")
+	result, err = contract.EvaluateTransaction("ReadAsset", "a1da243e-42c6-4c2d-86fe-5c44d79ea20d")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v\n", err)
+	}
+	log.Println(string(result))
+
+	log.Println("--> Evaluate Transaction: AssetExists, function returns 'true' if an asset with given assetID exist")
+	result, err = contract.EvaluateTransaction("AssetExists", "65fbb875-7f0d-4cf3-824c-d8b8977c7983")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v\n", err)
+	}
+	log.Println(string(result))
+
+	log.Println("--> Evaluate Transaction: WithDrawAsset, function returns 'a1da243e-42c6-4c2d-86fe-5c44d79ea20d' attributes")
+	result, err = contract.EvaluateTransaction("WithDrawAsset", "a1da243e-42c6-4c2d-86fe-5c44d79ea20d", "5")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v", err)
+	}
+	log.Println(string(result))
+
+	log.Println("--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger")
+	result, err = contract.EvaluateTransaction("GetAllAssets")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v", err)
+	}
+	log.Println(string(result))
+
+	log.Println("============ application-golang ends ============")
+}
+
+func populateWallet(wallet *gateway.Wallet) error {
+	log.Println("============ Populating wallet ============")
+	credPath := filepath.Join(
+		"..",
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"users",
+		"User1@org1.example.com",
+		"msp",
+	)
+
+	certPath := filepath.Join(credPath, "signcerts", "cert.pem")
+	// read the certificate pem
+	cert, err := ioutil.ReadFile(filepath.Clean(certPath))
+	if err != nil {
+		return err
+	}
+
+	keyDir := filepath.Join(credPath, "keystore")
+	// there's a single file in this dir containing the private key
+	files, err := ioutil.ReadDir(keyDir)
+	if err != nil {
+		return err
+	}
+	if len(files) != 1 {
+		return fmt.Errorf("keystore folder should have contain one file")
+	}
+	keyPath := filepath.Join(keyDir, files[0].Name())
+	key, err := ioutil.ReadFile(filepath.Clean(keyPath))
+	if err != nil {
+		return err
+	}
+
+	identity := gateway.NewX509Identity("Org1MSP", string(cert), string(key))
+
+	return wallet.Put("appUser", identity)
+}
